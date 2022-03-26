2a84dc2698a0949369948ea6e61852ebfcb4bc31
wozniackia <wozniackia@gmail.com>
2022-03-26 23:43:26 +0100

    added loading on book add

--- frontend/js/add-book.js
+++ frontend/js/add-book.js
@@ -3,38 +3,37 @@ function hideModal () {
 }
 
 function handleFormSubmit(event) {
-  document.getElementById('listName').classList.add("placeholder");
-  document.getElementById('listAuthor').classList.add("placeholder");
-  document.getElementById('listDate').classList.add("placeholder");
-  document.getElementById('buttonRequest').classList.add("placeholder");
+  button.innerHTML = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>'
+  // document.getElementById('listName').classList.add("placeholder");
+  // document.getElementById('listAuthor').classList.add("placeholder");
+  // document.getElementById('listDate').classList.add("placeholder");
+  // document.getElementById('buttonRequest').classList.add("placeholder");
   event.preventDefault();
-  console.log(bookName.value)
   let uri = 'https://openlibrary.org/search.json?q='+bookName.value;
-  console.log(uri)
   axios.get(uri)
     .then(function (response) {
+      button.innerHTML = 'Search'
       sendRequest = () => {
-        console.log(String(response.data.docs[0]["title"]).replaceAll(' ','+'))
+        document.getElementById('addButton').innerHTML = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>'
         let uri2 = 'https://wozniacki-booksapp.herokuapp.com/books/addBook?name='+String(response.data.docs[0]["title"]).replaceAll(' ','+')
-        console.log(uri2)
         axios.post(uri2)
           .then(function (response) {
-            console.log('yupi')
+            document.getElementById('addButton').innerHTML = 'Add'
             modal.style.display = "block";
           })
       }
-      document.getElementById('listName').classList.remove("placeholder");
-      document.getElementById('listAuthor').classList.remove("placeholder");
-      document.getElementById('listDate').classList.remove("placeholder");
-      document.getElementById('buttonRequest').classList.remove("placeholder");
+      // document.getElementById('listName').classList.remove("placeholder");
+      // document.getElementById('listAuthor').classList.remove("placeholder");
+      // document.getElementById('listDate').classList.remove("placeholder");
+      // document.getElementById('buttonRequest').classList.remove("placeholder");
       document.getElementById('listName').innerHTML = response.data.docs[0]["title"]
       document.getElementById('listAuthor').innerHTML = response.data.docs[0]["author_name"][0]
       document.getElementById('listDate').innerHTML = response.data.docs[0]["first_publish_year"]
-      document.getElementById('buttonRequest').onclick = sendRequest
-      lista.classList.remove("visually-hidden");
+      document.getElementById('addButton').onclick = sendRequest
     });
 }
 
+const button = document.getElementById('searchButton')
 const lista = document.getElementById('lista')
 const bookName = document.getElementById('bookName')
 const formularz = document.getElementById('formularz')
