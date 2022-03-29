5c6f9656e25eec481b05c4640310e0acd27ebee4
wozniackia <wozniackia@gmail.com>
2022-03-29 23:12:22 +0200

    darkmode on home page

---
+++ frontend/js/dark-mode.js
@@ -0,0 +1,66 @@
+localStorage.setItem('darkmode', 'true');
+
+function darkMode() {
+    let ifDark = localStorage.getItem('darkmode')
+    if(ifDark == 'true') {
+        localStorage.setItem('darkmode', 'false')
+        let dark = document.getElementsByClassName('bg-light')
+        for(let i = 0; i < dark.length; i++) {
+            dark[i].classList.add('bg-dark')
+        }
+        while(dark.length > 0) {
+            dark[0].classList.remove('bg-light')
+        }
+        dark = document.getElementsByClassName('navbar-light')
+        for(let i = 0; i < dark.length; i++) {
+            dark[i].classList.add('navbar-dark')
+        }
+        while(dark.length > 0) {
+            dark[0].classList.remove('navbar-light')
+        }
+        dark = document.getElementsByClassName('btn-light')
+        for(let i = 0; i < dark.length; i++) {
+            dark[i].classList.add('btn-dark')
+        }
+        while(dark.length > 0) {
+            dark[0].classList.remove('btn-light')
+        }
+        dark = document.getElementsByClassName('text-dark')
+        for(let i = 0; i < dark.length; i++) {
+            dark[i].classList.add('text-light')
+        }
+        while(dark.length > 0) {
+            dark[0].classList.remove('text-dark')
+        }
+    } else {
+        localStorage.setItem('darkmode', 'true');
+        let dark = document.getElementsByClassName('bg-dark')
+        for(let i = 0; i < dark.length; i++) {
+            dark[i].classList.add('bg-light')
+        }
+        while(dark.length > 0) {
+            dark[0].classList.remove('bg-dark')
+        }
+        dark = document.getElementsByClassName('navbar-dark')
+        for(let i = 0; i < dark.length; i++) {
+            dark[i].classList.add('navbar-light')
+        }
+        while(dark.length > 0) {
+            dark[0].classList.remove('navbar-dark')
+        }
+        dark = document.getElementsByClassName('btn-dark')
+        for(let i = 0; i < dark.length; i++) {
+            dark[i].classList.add('btn-light')
+        }
+        while(dark.length > 0) {
+            dark[0].classList.remove('btn-dark')
+        }
+        dark = document.getElementsByClassName('text-light')
+        for(let i = 0; i < dark.length; i++) {
+            dark[i].classList.add('text-dark')
+        }
+        while(dark.length > 0) {
+            dark[0].classList.remove('text-light')
+        }
+    }
+}
--- server.js
+++ server.js
@@ -5,31 +5,35 @@ const port = 3000
 const booksRouter = require('./routers/books')
 const dbo = require('./db/connection')
 
-app.set("view engine", "ejs");
-app.set('views', __dirname + '/frontend/views');
+app.set("view engine", "ejs")
+app.set('views', __dirname + '/frontend/views')
 
-app.use(express.static(__dirname));
-app.use(cors());
-app.use(express.json());
+app.use(express.static(__dirname))
+app.use(cors())
+app.use(express.json())
 app.use('/books', booksRouter)
 
 app.get('/', (req, res) => {
   res.render('index')
-})
+});
 app.get('/browse', (req, res) => {
   res.render('browse')
-})
+});
 app.get('/add', (req, res) => {
   res.render('add')
-})
+});
 app.get('/search', (req, res) => {
   res.render('search')
-})
+});
+
+app.get('/*', (req, res) => {
+  res.render('404')
+});
 
 dbo.connectToServer(function (err) {
     if (err) {
-      console.error(err);
-      process.exit();
+      console.error(err)
+      process.exit()
     }
   
     // start the Express server
