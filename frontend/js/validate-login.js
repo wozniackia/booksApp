61671f8b6fa4668f706a7a4881069a6fcc6549e9
wozniackia <wozniackia@gmail.com>
2023-01-20 10:11:22 +0100

    quick fix

--- frontend/js/validate-login.js
+++ frontend/js/validate-login.js
@@ -4,33 +4,31 @@ const isValid = () => {
         singinLink.innerHTML = sessionStorage.getItem("token-username");
     }
 
-    setInterval(() => {
-        if (sessionStorage.getItem("token") != null) {
-            axios.get(`/auth/validate?token=${sessionStorage.getItem("token")}`)
-                .then(function (response) {
-                    if (response.status == 400) {
-                        singinLink.innerHTML = sessionStorage.getItem("token-username");
-                        sessionStorage.removeItem("token");
-                        sessionStorage.removeItem("token-datetime");
-                        sessionStorage.removeItem("token-username");
-                    } else {
-                        if(document.title == "BooksApp - Sign in") {
-                            document.getElementById('signout').classList.remove('visually-hidden');
-                            document.getElementById('to-hide').classList.add('visually-hidden');
-                            document.getElementById('username').innerHTML = sessionStorage.getItem("token-username");
-                        } else if(document.title == "BooksApp - Add books") {
-                            document.getElementById('signedonly').classList.add('visually-hidden')
-                        }
-                    }
-                })
-                .catch(function (error) {
+    if (sessionStorage.getItem("token") != null) {
+        axios.get(`/auth/validate?token=${sessionStorage.getItem("token")}`)
+            .then(function (response) {
+                if (response.status == 400) {
                     singinLink.innerHTML = sessionStorage.getItem("token-username");
                     sessionStorage.removeItem("token");
                     sessionStorage.removeItem("token-datetime");
                     sessionStorage.removeItem("token-username");
-                });
-        }
-    }, 1000);
+                } else {
+                    if(document.title == "BooksApp - Sign in") {
+                        document.getElementById('signout').classList.remove('visually-hidden');
+                        document.getElementById('to-hide').classList.add('visually-hidden');
+                        document.getElementById('username').innerHTML = sessionStorage.getItem("token-username");
+                    } else if(document.title == "BooksApp - Add books") {
+                        document.getElementById('signedonly').classList.add('visually-hidden')
+                    }
+                }
+            })
+            .catch(function (error) {
+                singinLink.innerHTML = sessionStorage.getItem("token-username");
+                sessionStorage.removeItem("token");
+                sessionStorage.removeItem("token-datetime");
+                sessionStorage.removeItem("token-username");
+            });
+    }
 }
 
 isValid();
