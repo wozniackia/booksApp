c1ce7054dc01061c4789b919a0888a2652cb6089
wozniackia <wozniackia@gmail.com>
2023-06-13 09:02:03 +0200

    fix obrazkow

--- client/src/components/BookCard.js
+++ client/src/components/BookCard.js
@@ -4,10 +4,10 @@ import "../css/bootstrap.min.css"
 
 function BookCard() {
   return (
-    <div className="col" >
+    <div className="col" style={{width: "400px", height: "500px", marginBottom: "100px"}} >
       <div className="card shadow-sm">
         <div className="img-wrapper">
-          <img src="" alt="Book cover" className="cover"></img>
+          <img src="" style={{width: "380px", height:"500px"}} alt="Book cover" className="cover"></img>
         </div>
 
         <div className="card-body">
